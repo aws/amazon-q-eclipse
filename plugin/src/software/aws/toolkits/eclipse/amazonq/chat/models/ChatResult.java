12ffea19f0754efb2542d733c6fe9a0c3793eb72
Jordan Ang <angjordn@amazon.com>
2024-09-13 18:48:27 -0700

    Remove comment about type reference

--- plugin/src/software/aws/toolkits/eclipse/amazonq/chat/models/ChatResult.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/chat/models/ChatResult.java
@@ -5,9 +5,6 @@ package software.aws.toolkits.eclipse.amazonq.chat.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record ChatResult (
-    /* 
-     * See referenced type https://github.com/aws/aws-toolkit-visual-studio-staging/blob/9ed86affe1027a7fd62fa40bf05d2e130aec47cc/vspackages/AmazonQ/AwsToolkit.AmazonQ.Shared/Chat/Lsp/Protocols/ChatServerProtocols.cs#L32-L51
-     */
     @JsonProperty("body") String body,
     @JsonProperty("messageId") String messageId,
     @JsonProperty("canBeVoted") Boolean canBeVoted,

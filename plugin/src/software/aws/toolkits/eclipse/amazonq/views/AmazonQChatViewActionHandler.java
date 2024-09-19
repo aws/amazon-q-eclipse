16fd9269c917310b998526fd40c069cac11251da
Jordan Ang <angjordn@amazon.com>
2024-09-19 16:03:46 -0700

    Rendering chat response in UI

--- plugin/src/software/aws/toolkits/eclipse/amazonq/views/AmazonQChatViewActionHandler.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/views/AmazonQChatViewActionHandler.java
@@ -6,16 +6,22 @@ package software.aws.toolkits.eclipse.amazonq.views;
 import org.eclipse.swt.browser.Browser;
 
 import software.aws.toolkits.eclipse.amazonq.chat.ChatCommunicationManager;
+import software.aws.toolkits.eclipse.amazonq.chat.models.ChatRequestParams;
 import software.aws.toolkits.eclipse.amazonq.chat.models.ChatResult;
+import software.aws.toolkits.eclipse.amazonq.chat.models.ChatUIInboundCommand;
+import software.aws.toolkits.eclipse.amazonq.chat.models.ChatUIInboundCommandName;
 import software.aws.toolkits.eclipse.amazonq.exception.AmazonQPluginException;
+import software.aws.toolkits.eclipse.amazonq.util.JsonHandler;
 import software.aws.toolkits.eclipse.amazonq.util.PluginLogger;
 import software.aws.toolkits.eclipse.amazonq.views.model.Command;
 import software.aws.toolkits.eclipse.amazonq.views.model.ParsedCommand;
 
 public class AmazonQChatViewActionHandler implements ViewActionHandler {
     private ChatCommunicationManager chatCommunicationManager;
+    private final JsonHandler jsonHandler;
 
     public AmazonQChatViewActionHandler() {
+        this.jsonHandler = new JsonHandler();
         chatCommunicationManager = new ChatCommunicationManager();
     }
 
@@ -23,24 +29,44 @@ public class AmazonQChatViewActionHandler implements ViewActionHandler {
     public final void handleCommand(final ParsedCommand parsedCommand, final Browser browser) {
         Command command = parsedCommand.getCommand();
         Object params = parsedCommand.getParams();
+        String jsonParams = jsonHandler.serialize(params);
 
         PluginLogger.info(command + " being processed by ActionHandler");
 
         switch (command) {
             case CHAT_SEND_PROMPT:
-                ChatResult chatResult = chatCommunicationManager.sendMessageToChatServerAsync(command, params);
-                PluginLogger.info("Chat result: " + chatResult);
+                ChatResult chatResult = chatCommunicationManager.sendMessageToChatServer(command, params);
+                ChatRequestParams chatRequestParams = jsonHandler.deserialize(jsonParams, ChatRequestParams.class);
+                ChatUIInboundCommand chatUIInboundCommand = new ChatUIInboundCommand(
+                    ChatUIInboundCommandName.ChatPrompt.toString(),
+                    chatRequestParams.tabId(),
+                    chatResult,
+                    false
+                );
+                sendMessageToUI(browser, chatUIInboundCommand);
                 break;
             case CHAT_READY:
-                chatCommunicationManager.sendMessageToChatServerAsync(command, params);
+                chatCommunicationManager.sendMessageToChatServer(command, params);
                 break;
             case CHAT_TAB_ADD:
-                chatCommunicationManager.sendMessageToChatServerAsync(command, params);
+                chatCommunicationManager.sendMessageToChatServer(command, params);
                 break;
             case TELEMETRY_EVENT:
                 break;
             default:
                 throw new AmazonQPluginException("Unhandled command in AmazonQChatViewActionHandler: " + command.toString());
         }
+    }
+    
+    private void sendMessageToUI(Browser browser, ChatUIInboundCommand command) {
+        String message = this.jsonHandler.serialize(command);
+        PluginLogger.info("Sending message to UI: " + message);
+
+        String script = "window.postMessage(" + message + ");";
+        PluginLogger.info("Script: " + script);
+
+        browser.getDisplay().asyncExec(() -> {
+            browser.evaluate(script);
+        });
     }
 }

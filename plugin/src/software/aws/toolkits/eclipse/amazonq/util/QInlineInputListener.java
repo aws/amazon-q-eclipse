c4d3ad0c8333376eb7399fcc7634b4feec56bb9c
Felix Ding <dingfeli@amazon.com>
2024-09-25 12:21:42 -0700

    Refines typeahead logic with regards to auto closing brackets

--- plugin/src/software/aws/toolkits/eclipse/amazonq/util/QInlineInputListener.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/util/QInlineInputListener.java
@@ -2,8 +2,6 @@
 
 package software.aws.toolkits.eclipse.amazonq.util;
 
-import java.util.Stack;
-
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.swt.SWT;
@@ -15,8 +13,17 @@ import org.eclipse.swt.events.VerifyListener;
 public final class QInlineInputListener implements VerifyListener, VerifyKeyListener {
 	private StyledText widget = null;
 	private int distanceTraversed = 0;
-	private boolean isLastKeyBackspace = false;
 	private boolean isAutoClosingEnabled = true;
+	private LastKeyStrokeType lastKeyStrokeType = LastKeyStrokeType.NORMAL_INPUT;
+	
+	private enum LastKeyStrokeType {
+	    NORMAL_INPUT,
+	    BACKSPACE,
+	    NORMAL_BRACKET,
+	    CURLY_BRACES,
+	    OPEN_CURLY,
+	    OPEN_CURLY_FOLLOWED_BY_NEW_LINE, 
+	}
 
 	public QInlineInputListener(final StyledText widget) {
 	    IEclipsePreferences preferences = InstanceScope.INSTANCE.getNode("org.eclipse.jdt.ui");
@@ -52,13 +59,20 @@ public final class QInlineInputListener implements VerifyListener, VerifyKeyList
 		// - BS (backspace)
 		String currentSuggestion = qInvocationSessionInstance.getCurrentSuggestion().trim();
 		switch (event.keyCode) {
+		case SWT.CR:
+            if (lastKeyStrokeType == LastKeyStrokeType.OPEN_CURLY && isAutoClosingEnabled) {
+                lastKeyStrokeType = LastKeyStrokeType.OPEN_CURLY_FOLLOWED_BY_NEW_LINE;
+            } else {
+                lastKeyStrokeType = LastKeyStrokeType.NORMAL_INPUT;
+            }
+		    return;
 		case SWT.BS:
 			if (--distanceTraversed < 0) {
 				qInvocationSessionInstance.transitionToDecisionMade();
 				qInvocationSessionInstance.end();
 				return;
 			}
-			isLastKeyBackspace = true;
+			lastKeyStrokeType = LastKeyStrokeType.BACKSPACE;
 			return;
 		case SWT.ESC:
 			qInvocationSessionInstance.transitionToDecisionMade();
@@ -68,6 +82,7 @@ public final class QInlineInputListener implements VerifyListener, VerifyKeyList
 		}
 		
 		// If auto closing of brackets are not enabled we can just treat them as normal inputs
+		// Another scenario 
         if (!isAutoClosingEnabled) {
             return;
         }
@@ -75,79 +90,95 @@ public final class QInlineInputListener implements VerifyListener, VerifyKeyList
         // If auto cloising of brackets are enabled, SWT will treat the open bracket differently.
         // Input of the brackets will not trigger a call to verifyText. 
         // Thus we have to do the typeahead verification here. 
+        // Note that '{' is excluded because 
 		switch (event.character) {
         case '<':
             if (currentSuggestion.charAt(distanceTraversed++) != '<') {
-                System.out.println("bracket mismatched\ntyped: " + event.character + "\nsuggestion: " + currentSuggestion.charAt(distanceTraversed));
                 qInvocationSessionInstance.transitionToDecisionMade();
                 qInvocationSessionInstance.end();
                 return;
             }
+            lastKeyStrokeType = LastKeyStrokeType.NORMAL_BRACKET;
             return;
         case '>':
             if (currentSuggestion.charAt(distanceTraversed++) != '>') {
-                System.out.println("bracket mismatched\ntyped: " + event.character + "\nsuggestion: " + currentSuggestion.charAt(distanceTraversed));
                 qInvocationSessionInstance.transitionToDecisionMade();
                 qInvocationSessionInstance.end();
                 return;
             }
+            lastKeyStrokeType = LastKeyStrokeType.NORMAL_BRACKET;
             return;
         case '(':
             if (currentSuggestion.charAt(distanceTraversed++) != '(') {
-                System.out.println("bracket mismatched\ntyped: " + event.character + "\nsuggestion: " + currentSuggestion.charAt(distanceTraversed));
                 qInvocationSessionInstance.transitionToDecisionMade();
                 qInvocationSessionInstance.end();
                 return;
             }
+            lastKeyStrokeType = LastKeyStrokeType.NORMAL_BRACKET;
             return;
         case ')':
             if (currentSuggestion.charAt(distanceTraversed++) != ')') {
-                System.out.println("bracket mismatched\ntyped: " + event.character + "\nsuggestion: " + currentSuggestion.charAt(distanceTraversed));
                 qInvocationSessionInstance.transitionToDecisionMade();
                 qInvocationSessionInstance.end();
                 return;
             }
+            lastKeyStrokeType = LastKeyStrokeType.NORMAL_BRACKET;
             return;
         case '[':
             if (currentSuggestion.charAt(distanceTraversed++) != '[') {
-                System.out.println("bracket mismatched\ntyped: " + event.character + "\nsuggestion: " + currentSuggestion.charAt(distanceTraversed));
                 qInvocationSessionInstance.transitionToDecisionMade();
                 qInvocationSessionInstance.end();
                 return;
             }
+            lastKeyStrokeType = LastKeyStrokeType.NORMAL_BRACKET;
             return;
         case ']':
             if (currentSuggestion.charAt(distanceTraversed++) != ']') {
-                System.out.println("bracket mismatched\ntyped: " + event.character + "\nsuggestion: " + currentSuggestion.charAt(distanceTraversed));
                 qInvocationSessionInstance.transitionToDecisionMade();
                 qInvocationSessionInstance.end();
                 return;
             }
+            lastKeyStrokeType = LastKeyStrokeType.NORMAL_BRACKET;
             return;
         case '{':
             if (currentSuggestion.charAt(distanceTraversed++) != '{') {
-                System.out.println("bracket mismatched\ntyped: " + event.character + "\nsuggestion: " + currentSuggestion.charAt(distanceTraversed));
                 qInvocationSessionInstance.transitionToDecisionMade();
                 qInvocationSessionInstance.end();
                 return;
             }
+            lastKeyStrokeType = LastKeyStrokeType.OPEN_CURLY;
             return;
         case '}':
             if (currentSuggestion.charAt(distanceTraversed++) != '}') {
-                System.out.println("bracket mismatched\ntyped: " + event.character + "\nsuggestion: " + currentSuggestion.charAt(distanceTraversed));
                 qInvocationSessionInstance.transitionToDecisionMade();
                 qInvocationSessionInstance.end();
                 return;
             }
+            lastKeyStrokeType = LastKeyStrokeType.CURLY_BRACES;
             return;
+        case '"':
+            if (currentSuggestion.charAt(distanceTraversed++) != '"') {
+                qInvocationSessionInstance.transitionToDecisionMade();
+                qInvocationSessionInstance.end();
+                return;
+            }
+            lastKeyStrokeType = LastKeyStrokeType.NORMAL_BRACKET;
         default:
         }
+		
+		lastKeyStrokeType = LastKeyStrokeType.NORMAL_INPUT;
 	}
 
 	@Override
 	public void verifyText(final VerifyEvent event) {
-	    if (isLastKeyBackspace) {
-	        isLastKeyBackspace = false;
+	    String input = event.text;
+	    switch (lastKeyStrokeType) {
+	    case NORMAL_INPUT:
+	        break;
+	    case OPEN_CURLY_FOLLOWED_BY_NEW_LINE:
+	        input = '\n' + event.text.split("\\R")[1];
+	        break;
+	    default:
 	        return;
 	    }
 
@@ -169,26 +200,31 @@ public final class QInlineInputListener implements VerifyListener, VerifyKeyList
 
 		System.out.println("=========================\nDistance traversed: " + distanceTraversed);
 		System.out.println("text typed: " + event.text);
-		System.out.println("current char in suggestion: " + currentSuggestion.charAt(distanceTraversed));
 		System.out.println("Current caret offset: " + currentOffset);
 		System.out.println("Is auto closing brackets enabled: " + isAutoClosingEnabled);
 
 		boolean isOutOfBounds = distanceTraversed >= currentSuggestion.length() || distanceTraversed < 0;
-		if (isOutOfBounds || !isInputAMatch(currentSuggestion, distanceTraversed, event.text)) {
+		if (!isOutOfBounds) {
+		    System.out.println("current char in suggestion: " + currentSuggestion.charAt(distanceTraversed));
+		}
+		if (isOutOfBounds || !isInputAMatch(currentSuggestion, distanceTraversed, input)) {
 			qInvocationSessionInstance.transitionToDecisionMade();
 			qInvocationSessionInstance.end();
 			return;
 		}
+		distanceTraversed += input.length();
 	}
-	
+
 	private boolean isInputAMatch(String currentSuggestion, int startIdx, String input) {
 	    boolean res;
 		if (input.length() > 1) {
+		    System.out.println("Suggestion: " + currentSuggestion.substring(startIdx, startIdx + input.length()).replace("\t", "\\t").replace("\n", "\\n").replace("\r", "\\r"));
+		    System.out.println("Adjusted input: " + input.replace("\t", "\\t").replace("\n", "\\n").replace("\r", "\\r"));
 			res = currentSuggestion.substring(startIdx, startIdx + input.length()).equals(input);
+			System.out.println("This is a match: " + res);
 		} else {
 			res = String.valueOf(currentSuggestion.charAt(startIdx)).equals(input);
 		}
-		distanceTraversed += input.length();
 		return res;
 	}
 }

6a25a1002737487fc282f2d2e0d41317bf0f390b
Jordan Ang <angjordn@amazon.com>
2024-10-07 16:50:12 -0700

    Define GenericCommandParams

---
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/chat/models/GenericCommandParams.java
@@ -0,0 +1,12 @@
+// Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+package software.aws.toolkits.eclipse.amazonq.chat.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record GenericCommandParams(
+        @JsonProperty("genericCommand") String genericCommand,
+        @JsonProperty("selection") String selection,
+        @JsonProperty("triggerType") String triggerType,
+) { };
+

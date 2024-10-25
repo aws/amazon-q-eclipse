08266aea4247128cf36bbd10ab737597e5d8ac5f
Jordan Ang <angjordn@amazon.com>
2024-10-25 14:43:22 -0700

    Add Reauthenticate view without handlers

---
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/views/ReauthenticateView.java
@@ -0,0 +1,62 @@
+package software.aws.toolkits.eclipse.amazonq.views;
+
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+
+
+public class ReauthenticateView extends CallToActionView {
+	public static final String ID = "software.aws.toolkits.eclipse.amazonq.views.ReauthenticateView";
+	
+	private static final String ICON_PATH = "icons/AmazonQ64.png";
+	private static final String HEADER_LABEL = "Connection to Amazon Q Expired";
+	private static final String DETAIL_MESSAGE = "Please re-authenticate to continue";
+	private static final String BUTTON_LABEL = "Re-authenticate";
+	private static final String LINK_LABEL = "Sign out";
+
+	@Override
+	protected String getIconPath() {
+		return ICON_PATH;
+	}
+
+	@Override
+	protected String getHeaderLabel() {
+		return HEADER_LABEL;
+	}
+
+	@Override
+	protected String getDetailMessage() {
+		return DETAIL_MESSAGE;
+	}
+
+	@Override
+	protected String getButtonLabel() {
+		return BUTTON_LABEL;
+	}
+
+	@Override
+	protected SelectionListener getButtonHandler() {
+        return new SelectionAdapter() {
+            @Override
+            public void widgetSelected(final SelectionEvent e) {
+            	// TODO
+            }
+        };
+	}
+
+	@Override
+	protected String getLinkLabel() {
+		return LINK_LABEL;
+	}
+
+	@Override
+	protected SelectionListener getLinkHandler() {
+        return new SelectionAdapter() {
+            @Override
+            public void widgetSelected(final SelectionEvent e) {
+            	// TODO
+            }
+        };
+	}
+
+}

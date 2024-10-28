423fe22a97034fa9d3651736a7065df27d84cfaf
Ishan Taldekar <taldekar@amazon.com>
2024-10-28 16:24:28 -0400

    Demonstrate safer use of static mocks using extensions

--- plugin/tst/software/aws/toolkits/eclipse/amazonq/extensions/ActivatorStaticMockExtension.java
+++ plugin/tst/software/aws/toolkits/eclipse/amazonq/extensions/ActivatorStaticMockExtension.java
@@ -10,12 +10,9 @@ import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
-import software.aws.toolkits.eclipse.amazonq.configuration.PluginStore;
 import software.aws.toolkits.eclipse.amazonq.plugin.Activator;
-import software.aws.toolkits.eclipse.amazonq.providers.LspProvider;
 import software.aws.toolkits.eclipse.amazonq.telemetry.service.TelemetryService;
 import software.aws.toolkits.eclipse.amazonq.util.LoggingService;
-import software.aws.toolkits.eclipse.amazonq.util.LoginService;
 
 import java.util.Optional;
 
@@ -27,9 +24,6 @@ public final class ActivatorStaticMockExtension implements BeforeAllCallback, Af
     @Mock private static TelemetryService telemetryServiceMock;
     @Mock private static LoggingService loggingServiceMock;
     @Mock private static Activator activatorMock;
-    @Mock private static LspProvider lspProviderMock;
-    @Mock private static LoginService loginServiceMock;
-    @Mock private static PluginStore pluginStoreMock;
 
     private static final Map<Class<?>, Object> MOCKS_MAP;
 
@@ -37,27 +31,12 @@ public final class ActivatorStaticMockExtension implements BeforeAllCallback, Af
         telemetryServiceMock = Mockito.mock(TelemetryService.class);
         loggingServiceMock = Mockito.mock(LoggingService.class);
         activatorMock = Mockito.mock(Activator.class);
-        lspProviderMock = Mockito.mock(LspProvider.class);
-        loginServiceMock = Mockito.mock(LoginService.class);
-        pluginStoreMock = Mockito.mock(PluginStore.class);
 
         MOCKS_MAP = Map.of(
             TelemetryService.class, telemetryServiceMock,
             LoggingService.class, loggingServiceMock,
-            Activator.class, activatorMock,
-            LspProvider.class, lspProviderMock,
-            LoginService.class, loginServiceMock,
-            PluginStore.class, pluginStoreMock
+            Activator.class, activatorMock
         );
-    }
-
-    @SuppressWarnings("unchecked")
-    public static <T> Optional<T> getMock(final Class<T> type) {
-        return Optional.ofNullable(MOCKS_MAP.get(type)).map(mock -> (T) mock);
-    }
-
-    public static MockedStatic<Activator> getStaticMock() {
-        return activatorStaticMock;
     }
 
     @Override
@@ -66,9 +45,11 @@ public final class ActivatorStaticMockExtension implements BeforeAllCallback, Af
         activatorStaticMock.when(Activator::getLogger).thenReturn(loggingServiceMock);
         activatorStaticMock.when(Activator::getTelemetryService).thenReturn(telemetryServiceMock);
         activatorStaticMock.when(Activator::getDefault).thenReturn(activatorMock);
-        activatorStaticMock.when(Activator::getLspProvider).thenReturn(lspProviderMock);
-        activatorStaticMock.when(Activator::getLoginService).thenReturn(loginServiceMock);
-        activatorStaticMock.when(Activator::getPluginStore).thenReturn(pluginStoreMock);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> Optional<T> getMock(final Class<T> type) {
+        return Optional.ofNullable(MOCKS_MAP.get(type)).map(mock -> (T) mock);
     }
 
     @Override

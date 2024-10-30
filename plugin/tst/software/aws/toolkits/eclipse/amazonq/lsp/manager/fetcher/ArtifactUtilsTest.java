6814b3cbc5916eae9fb189b7915abaf427a4b5be
Shruti Sinha <shrsinh@amazon.com>
2024-10-30 10:08:08 -0700

    fix test checkstyle

--- plugin/tst/software/aws/toolkits/eclipse/amazonq/lsp/manager/fetcher/ArtifactUtilsTest.java
+++ plugin/tst/software/aws/toolkits/eclipse/amazonq/lsp/manager/fetcher/ArtifactUtilsTest.java
@@ -15,7 +15,6 @@ import java.util.zip.ZipOutputStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ArtifactUtilsTest {

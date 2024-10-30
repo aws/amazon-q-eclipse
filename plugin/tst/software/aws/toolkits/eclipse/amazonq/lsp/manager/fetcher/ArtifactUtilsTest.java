552bf9cb109e88392b8f29f00db6da91e390b05a
Shruti Sinha <shrsinh@amazon.com>
2024-10-30 11:31:27 -0700

    temporarily disable lsp manager tests for fixing

--- plugin/tst/software/aws/toolkits/eclipse/amazonq/lsp/manager/fetcher/ArtifactUtilsTest.java
+++ plugin/tst/software/aws/toolkits/eclipse/amazonq/lsp/manager/fetcher/ArtifactUtilsTest.java
@@ -15,7 +15,6 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 import java.util.Collections;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;

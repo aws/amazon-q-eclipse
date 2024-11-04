1126b540ae053a5292854de7f15e17d7b5de36bb
Felix Ding <dingfeli@amazon.com>
2024-11-04 15:13:20 -0800

    Adds command listener to autotrigger document listener

--- plugin/src/software/aws/toolkits/eclipse/amazonq/util/AutoTriggerDocumentListener.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/util/AutoTriggerDocumentListener.java
@@ -1,15 +1,51 @@
 // Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 package software.aws.toolkits.eclipse.amazonq.util;
 
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.IExecutionListener;
+import org.eclipse.core.commands.NotHandledException;
 import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.IDocumentListener;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.commands.ICommandService;
 
 import static software.aws.toolkits.eclipse.amazonq.util.QEclipseEditorUtils.getActiveTextEditor;
 
 import java.util.concurrent.ExecutionException;
 
 public final class AutoTriggerDocumentListener implements IDocumentListener, IAutoTriggerListener {
+    private static final String ACCEPTANCE_COMMAND_ID = "software.aws.toolkits.eclipse.amazonq.commands.acceptSuggestions";
+
+    private boolean isChangeInducedByAcceptance = false;
+    private IExecutionListener executionListener = null;
+
+    public AutoTriggerDocumentListener() {
+        ICommandService commandService = PlatformUI.getWorkbench().getService(ICommandService.class);
+        executionListener = new IExecutionListener() {
+            @Override
+            public void notHandled(final String commandId, final NotHandledException exception) {
+                return;
+            }
+            @Override
+            public void postExecuteFailure(final String commandId, final org.eclipse.core.commands.ExecutionException exception) {
+                return;
+            }
+            @Override
+            public void postExecuteSuccess(final String commandId, final Object returnValue) {
+                return;
+            }
+            @Override
+            public void preExecute(final String commandId, final ExecutionEvent event) {
+                if (commandId.equals(ACCEPTANCE_COMMAND_ID)) {
+                    System.out.println("command detected");
+                    isChangeInducedByAcceptance = true;
+                }
+            }
+        };
+        commandService.addExecutionListener(executionListener);
+    }
 
     @Override
     public void documentAboutToBeChanged(final DocumentEvent e) {
@@ -30,6 +66,7 @@ public final class AutoTriggerDocumentListener implements IDocumentListener, IAu
                 return;
             }
         }
+        System.out.println("making an auto query");
         qSes.invoke(qSes.getViewer().getTextWidget().getCaretOffset(), e.getText().length());
     }
 
@@ -38,7 +75,13 @@ public final class AutoTriggerDocumentListener implements IDocumentListener, IAu
             return false;
         }
 
-        if (session.isPreviewingSuggestions() || session.isDecisionMade()) {
+        if (session.isPreviewingSuggestions()) {
+            return false;
+        }
+
+        if (isChangeInducedByAcceptance) {
+            // It is acceptable to alter the state here because:
+            isChangeInducedByAcceptance = false;
             return false;
         }
 

2970682571f73f599df67da571143785e1a997af
Jordan Ang <angjordn@amazon.com>
2024-11-06 18:47:14 -0800

    Handle chat asset missing view

--- plugin/src/software/aws/toolkits/eclipse/amazonq/views/AmazonQChatWebview.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/views/AmazonQChatWebview.java
@@ -3,12 +3,6 @@
 
 package software.aws.toolkits.eclipse.amazonq.views;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Optional;
-
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.browser.BrowserFunction;
 import org.eclipse.swt.browser.ProgressAdapter;
@@ -16,20 +10,14 @@ import org.eclipse.swt.browser.ProgressEvent;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import software.aws.toolkits.eclipse.amazonq.chat.ChatCommunicationManager;
 import software.aws.toolkits.eclipse.amazonq.chat.ChatTheme;
-import software.aws.toolkits.eclipse.amazonq.lsp.AwsServerCapabiltiesProvider;
 import software.aws.toolkits.eclipse.amazonq.lsp.auth.model.LoginDetails;
 import software.aws.toolkits.eclipse.amazonq.lsp.auth.model.LoginType;
-import software.aws.toolkits.eclipse.amazonq.lsp.model.ChatOptions;
-import software.aws.toolkits.eclipse.amazonq.lsp.model.QuickActions;
-import software.aws.toolkits.eclipse.amazonq.lsp.model.QuickActionsCommandGroup;
 import software.aws.toolkits.eclipse.amazonq.plugin.Activator;
-import software.aws.toolkits.eclipse.amazonq.providers.LspManagerProvider;
+import software.aws.toolkits.eclipse.amazonq.util.ChatContentProvider;
 import software.aws.toolkits.eclipse.amazonq.util.ThreadingUtils;
-import software.aws.toolkits.eclipse.amazonq.util.WebviewAssetServer;
 import software.aws.toolkits.eclipse.amazonq.views.actions.AmazonQCommonActions;
 
 public class AmazonQChatWebview extends AmazonQView implements ChatUiRequestListener {
@@ -37,7 +25,7 @@ public class AmazonQChatWebview extends AmazonQView implements ChatUiRequestList
     public static final String ID = "software.aws.toolkits.eclipse.amazonq.views.AmazonQChatWebview";
 
     private AmazonQCommonActions amazonQCommonActions;
-    private WebviewAssetServer webviewAssetServer;
+    private ChatContentProvider chatContentProvider;
 
     private final ViewCommandParser commandParser;
     private final ViewActionHandler actionHandler;
@@ -50,6 +38,7 @@ public class AmazonQChatWebview extends AmazonQView implements ChatUiRequestList
         this.chatCommunicationManager = ChatCommunicationManager.getInstance();
         this.actionHandler = new AmazonQChatViewActionHandler(chatCommunicationManager);
         this.chatTheme = new ChatTheme();
+        this.chatContentProvider = new ChatContentProvider();
     }
 
     @Override
@@ -107,7 +96,7 @@ public class AmazonQChatWebview extends AmazonQView implements ChatUiRequestList
                 AmazonQView.showView(ReauthenticateView.ID);
             } else {
                 if (!browser.isDisposed()) {
-                    getContent().ifPresentOrElse(
+                    chatContentProvider.getContent().ifPresentOrElse(
                             content -> browser.setText(content),
                             this::showChatAssetMissingView
                         );
@@ -125,112 +114,6 @@ public class AmazonQChatWebview extends AmazonQView implements ChatUiRequestList
         }
     }
 
-    private Optional<String> getContent() {
-        var chatUiDirectory = LspManagerProvider.getInstance().getLspInstallation().getClientDirectory();
-
-        if (!isValid(chatUiDirectory)) {
-            Activator.getLogger().error("Error loading Chat UI. If override used, please verify the override env variables else restart Eclipse");
-            return Optional.empty();
-        }
-
-        String jsFile = Paths.get(chatUiDirectory).resolve("amazonq-ui.js").toString();
-        var jsParent = Path.of(jsFile).getParent();
-        var jsDirectoryPath = Path.of(jsParent.toUri()).normalize().toString();
-
-        webviewAssetServer = new WebviewAssetServer();
-        var result = webviewAssetServer.resolve(jsDirectoryPath);
-        if (!result) {
-            Activator.getLogger().error(String.format(
-                    "Error loading Chat UI. Unable to find the `amazonq-ui.js` file in the directory: %s. Please verify and restart",
-                    chatUiDirectory));
-            return Optional.empty();
-        }
-
-        var chatJsPath = webviewAssetServer.getUri() + "amazonq-ui.js";
-        return Optional.of(String.format("""
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <meta
-                        http-equiv="Content-Security-Policy"
-                        content="default-src 'none'; script-src %s 'unsafe-inline'; style-src %s 'unsafe-inline';
-                        img-src 'self' data:; object-src 'none'; base-uri 'none';"
-                    >
-                    <title>Chat UI</title>
-                    %s
-                </head>
-                <body>
-                    %s
-                </body>
-                </html>
-                """, chatJsPath, chatJsPath, generateCss(), generateJS(chatJsPath)));
-    }
-
-    private boolean isValid(final String chatUiDirectory) {
-        return chatUiDirectory != null && !chatUiDirectory.isEmpty() && Files.exists(Paths.get(chatUiDirectory));
-    }
-
-    private String generateCss() {
-        return """
-                <style>
-                    body,
-                    html {
-                        background-color: var(--mynah-color-bg);
-                        color: var(--mynah-color-text-default);
-                        height: 100vh;
-                        width: 100%%;
-                        overflow: hidden;
-                        margin: 0;
-                        padding: 0;
-                    }
-                    textarea:placeholder-shown {
-                        line-height: 1.5rem;
-                    }
-                </style>
-                """;
-    }
-
-    private String generateJS(final String jsEntrypoint) {
-        var chatQuickActionConfig = generateQuickActionConfig();
-        return String.format("""
-                <script type="text/javascript" src="%s" defer onload="init()"></script>
-                <script type="text/javascript">
-                    const init = () => {
-                        amazonQChat.createChat({
-                           postMessage: (message) => {
-                                ideCommand(JSON.stringify(message));
-                           }
-                        }, %s);
-                    }
-                </script>
-                """, jsEntrypoint, chatQuickActionConfig);
-    }
-
-    /*
-     * Generates javascript for chat options to be supplied to Chat UI defined here
-     * https://github.com/aws/language-servers/blob/785f8dee86e9f716fcfa29b2e27eb07a02387557/chat-client/src/client/chat.ts#L87
-     */
-    private String generateQuickActionConfig() {
-        return Optional.ofNullable(AwsServerCapabiltiesProvider.getInstance().getChatOptions())
-                .map(ChatOptions::quickActions)
-                .map(QuickActions::quickActionsCommandGroups)
-                .map(this::serializeQuickActionCommands)
-                .orElse("");
-    }
-
-    private String serializeQuickActionCommands(final List<QuickActionsCommandGroup> quickActionCommands) {
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            String json = mapper.writeValueAsString(quickActionCommands);
-            return String.format("{\"quickActionCommands\": %s}", json);
-        } catch (Exception e) {
-            Activator.getLogger().warn("Error occurred when json serializing quick action commands", e);
-            return "";
-        }
-    }
-
     @Override
     public final void onSendToChatUi(final String message) {
         var browser = getBrowser();
@@ -242,20 +125,12 @@ public class AmazonQChatWebview extends AmazonQView implements ChatUiRequestList
 
     @Override
     public final void dispose() {
-        if (webviewAssetServer != null) {
-            webviewAssetServer.stop();
-        }
+        chatContentProvider.dispose();
         chatCommunicationManager.removeListener();
         super.dispose();
     }
 
     private void showChatAssetMissingView() {
-        Display.getCurrent().asyncExec(() -> {
-            try {
-                showView(ChatAssetMissingView.ID);
-            } catch (Exception e) {
-                Activator.getLogger().error("Error occured while attempting to show chat asset missing view", e);
-            }
-        });
+        AmazonQView.showView(ChatAssetMissingView.ID);
     }
 }
--- plugin/src/software/aws/toolkits/eclipse/amazonq/views/ChatAssetMissingView.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/views/ChatAssetMissingView.java
@@ -3,12 +3,23 @@
 
 package software.aws.toolkits.eclipse.amazonq.views;
 
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+import software.aws.toolkits.eclipse.amazonq.util.ChatContentProvider;
+
 public final class ChatAssetMissingView extends BaseView {
     public static final String ID = "software.aws.toolkits.eclipse.amazonq.views.ChatAssetMissingView";
 
     private static final String ICON_PATH = "icons/AmazonQ64.png";
     private static final String HEADER_LABEL = "Error loading Q chat.";
     private static final String DETAIL_MESSAGE = "Restart Eclipse or review error logs for troubleshooting";
+    private ChatContentProvider chatContentProvider;
+
+    public ChatAssetMissingView() {
+        this.chatContentProvider = new ChatContentProvider();
+    }
+
     @Override
     protected String getIconPath() {
         return ICON_PATH;
@@ -24,4 +35,16 @@ public final class ChatAssetMissingView extends BaseView {
         return DETAIL_MESSAGE;
     }
 
+    @Override
+    protected CompletableFuture<Boolean> isViewDisplayable() {
+        return CompletableFuture.supplyAsync(() -> {
+            Optional<String> content = chatContentProvider.getContent();
+            return !content.isPresent();
+        });
+    }
+
+    @Override
+    protected void handleNonDisplayableView() {
+        AmazonQView.showView(AmazonQChatWebview.ID);
+    }
 }

8476b89e5ee753aba23aa179df453c782a0c9a91
Jordan Ang <angjordn@amazon.com>
2024-11-11 21:33:00 -0800

    Update asserts

--- plugin/tst/software/aws/toolkits/eclipse/amazonq/lsp/auth/AuthPluginStoreTest.java
+++ plugin/tst/software/aws/toolkits/eclipse/amazonq/lsp/auth/AuthPluginStoreTest.java
@@ -3,8 +3,8 @@
 
 package software.aws.toolkits.eclipse.amazonq.lsp.auth;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
--- plugin/tst/software/aws/toolkits/eclipse/amazonq/util/AuthUtilTest.java
+++ plugin/tst/software/aws/toolkits/eclipse/amazonq/util/AuthUtilTest.java
@@ -3,9 +3,10 @@
 
 package software.aws.toolkits.eclipse.amazonq.util;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 

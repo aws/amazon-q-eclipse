0f08ff3e2b71add75187b8fe647150c475a205b7
Nicolas Borges <nickdb@amazon.com>
2024-11-12 10:50:31 -0500

    remove unneeded imports

--- plugin/tst/software/aws/toolkits/eclipse/amazonq/lsp/manager/DefaultLspManagerTest.java
+++ plugin/tst/software/aws/toolkits/eclipse/amazonq/lsp/manager/DefaultLspManagerTest.java
@@ -47,9 +47,6 @@ import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 
 public class DefaultLspManagerTest {

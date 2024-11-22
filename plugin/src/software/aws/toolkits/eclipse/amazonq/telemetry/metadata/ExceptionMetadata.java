779f8238a545ead07fe0b66f72bda8d068b2d9eb
Nicolas Borges <nickdb@amazon.com>
2024-11-22 18:43:58 -0500

    fix checkstyle

--- plugin/src/software/aws/toolkits/eclipse/amazonq/telemetry/metadata/ExceptionMetadata.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/telemetry/metadata/ExceptionMetadata.java
@@ -3,17 +3,17 @@
 
 package software.aws.toolkits.eclipse.amazonq.telemetry.metadata;
 
-public class ExceptionMetadata {
+public final class ExceptionMetadata {
     private ExceptionMetadata() {
         //prevent instantiation
     }
-    public static String scrubException(Exception e) {
+    public static String scrubException(final Exception e) {
         /*TODO: add logic to scrub exception method of any senstive data or PII
          * Will return exception class name until scrubbing logic is implemented
          */
         return e.getClass().getName();
     }
-    public static String scrubException(String prefixString, Exception e) {
+    public static String scrubException(final String prefixString, final Exception e) {
         return prefixString + ". Error: " + scrubException(e);
     }
 }

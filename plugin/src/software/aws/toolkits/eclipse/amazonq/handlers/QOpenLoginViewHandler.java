77b3212750405dfb6ab8f2cc7f2fb8cd1ecc7acc
Ishan Taldekar <taldekar@amazon.com>
2024-12-06 14:28:01 -0500

    Fix multi-window chat bug

--- plugin/src/software/aws/toolkits/eclipse/amazonq/handlers/QOpenLoginViewHandler.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/handlers/QOpenLoginViewHandler.java
@@ -5,13 +5,22 @@ package software.aws.toolkits.eclipse.amazonq.handlers;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
-import software.aws.toolkits.eclipse.amazonq.plugin.Activator;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IViewReference;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 
+import software.aws.toolkits.eclipse.amazonq.plugin.Activator;
+import software.aws.toolkits.eclipse.amazonq.views.AmazonQView;
 import software.aws.toolkits.eclipse.amazonq.views.ViewVisibilityManager;
 
 public class QOpenLoginViewHandler extends AbstractHandler {
+
     @Override
     public final Object execute(final ExecutionEvent event) {
+        closeAnyOpenQViews();
+
         if (Activator.getLoginService().getAuthState().isLoggedIn()) {
             ViewVisibilityManager.showChatView("statusBar");
         } else {
@@ -19,4 +28,30 @@ public class QOpenLoginViewHandler extends AbstractHandler {
         }
         return null;
     }
+
+    public final void closeAnyOpenQViews() {
+        IWorkbenchWindow activeWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+        IWorkbenchWindow[] windows = PlatformUI.getWorkbench().getWorkbenchWindows();
+
+        for (IWorkbenchWindow window : windows) {
+            IWorkbenchPage[] pages = window.getPages();
+
+            for (IWorkbenchPage page : pages) {
+                IViewReference[] viewReferences = page.getViewReferences();
+
+                for (IViewReference viewRef : viewReferences) {
+                    IViewPart view = viewRef.getView(false);
+
+                    if (view instanceof AmazonQView) {
+                        page.hideView(view);
+                    }
+                }
+            }
+        }
+
+        if (activeWindow != null) {
+            activeWindow.getShell().setActive();
+        }
+    }
+
 }

d64500377095c03282ced56ddefcc376f8ac9b0f
Ishan Taldekar <taldekar@amazon.com>
2025-03-13 13:00:48 -0400

    Move global pulldown menu initialization to LspStartupActivity

--- plugin/src/software/aws/toolkits/eclipse/amazonq/plugin/Activator.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/plugin/Activator.java
@@ -20,8 +20,6 @@ import software.aws.toolkits.eclipse.amazonq.util.CodeReferenceLoggingService;
 import software.aws.toolkits.eclipse.amazonq.util.DefaultCodeReferenceLoggingService;
 import software.aws.toolkits.eclipse.amazonq.util.LoggingService;
 import software.aws.toolkits.eclipse.amazonq.util.PluginLogger;
-import software.aws.toolkits.eclipse.amazonq.views.actions.AmazonQAbstractCommonActions;
-import software.aws.toolkits.eclipse.amazonq.views.actions.AmazonQGlobalCommonActions;
 import software.aws.toolkits.eclipse.amazonq.views.router.ViewRouter;
 
 public class Activator extends AbstractUIPlugin {
@@ -36,7 +34,6 @@ public class Activator extends AbstractUIPlugin {
     private static PluginStore pluginStore;
     private static EventBroker eventBroker = new EventBroker();
     private static ViewRouter viewRouter = ViewRouter.builder().build();
-    private static AmazonQAbstractCommonActions globalCommonActions = AmazonQGlobalCommonActions.getInstance();
 
     public Activator() {
         super();

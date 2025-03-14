f4fcea022fb8ad527b3f6f2082934e7cc964f814
Ishan Taldekar <taldekar@amazon.com>
2025-03-14 11:19:28 -0400

    Fix copyright comment header

--- plugin/src/software/aws/toolkits/eclipse/amazonq/chat/ChatPartialResultMap.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/chat/ChatPartialResultMap.java
@@ -1,9 +1,14 @@
 // Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 package software.aws.toolkits.eclipse.amazonq.chat;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import org.eclipse.lsp4j.ProgressParams;
+
+import software.aws.toolkits.eclipse.amazonq.lsp.AmazonQLspClientImpl;
 
 
 /**

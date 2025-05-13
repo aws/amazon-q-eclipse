3c92ea808cc23d6be89d674838cbe6c6c207e69c
Ishan Taldekar <taldekar@amazon.com>
2025-05-12 22:59:03 -0400

    Run zoom once per browser

--- plugin/src/software/aws/toolkits/eclipse/amazonq/views/AmazonQChatWebview.java
+++ plugin/src/software/aws/toolkits/eclipse/amazonq/views/AmazonQChatWebview.java
@@ -59,8 +59,6 @@ public class AmazonQChatWebview extends AmazonQView implements ChatUiRequestList
             webViewAssetProvider.injectAssets(browser);
         }
 
-        browser.execute("document.body.style.zoom = '150%';");
-
         super.setupView(parent);
 
         parent.addDisposeListener(e -> this.preserveBrowser());
